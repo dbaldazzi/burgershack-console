c7467f460498258cb6fe511e194cd3dbceb99210
dbaldazzi <d.baldazzi@hotmail.com>
2019-10-04 16:36:56 -0600

    listingService done

--- Services/ListingService.cs
+++ Services/ListingService.cs
@@ -11,7 +11,42 @@ namespace burgershack2.Services
 
     public void Setup()
     {
-          Entree Steak = new Entree("")
+          Entree Steak = new Entree("T-Bone", "prime cut grilled to your liking", 15);
+          Entree Burger = new Entree("Cheese Burger", "1/4pounder with lettace, bacon and cheese", 10);
+          Entree Chicken = new Entree("Spicy Chicken Sandwich", "grilled chicken breast on bun", 12); 
+
+          Listings.AddRange(newListing[] {Steak, Burger, Chicken});
     }
+    internal void GetEntree(int index) 
+    {
+      if(index < Listing.Count && index > -1); 
+      {
+        Listing listing = Listing[index];
+        Messages.Add(listings.GetTemplate());
+        if(listing is Entree)
+        {
+        Entree EntreeListing = (Entree)Listing; 
+        }
+      }
+      else {
+        Messages.Add("invalid Choice");
+      }      
+    }
+    public void GetEntree() 
+    {
+      Messages.Add("Available Entrees");
+      for (int i=0; i< Entree.count; i++);
+      {
+      Listings Entree = Listings[i]; 
+      Messages.Add($"{i+1}: {Entree.name} - {Entree.Price}"); 
+      }
+   
+    Messages.Add("Type a number or q to quit"); 
+ }
+ public EntreeService()
+{
+Messages = new List<string>();
+Listings = new List<Listings>(); 
+}
   }
 }
